b5f366ca4e29f12b3f780177370853b6a451b3f6
Trini <trini.4595@gmail.com>
2022-01-22 19:23:48 -0300

    feat(portfolio): def Portfolio class and initialize it with stocks

--- app/solution.rb
+++ app/solution.rb
@@ -1,5 +1,12 @@
 class Stock
   def price(date = Time.now)
-    rand(1.0..100000.0).round(2)
+    rand(1.0..100000.0).round(2) # random price
+  end
+end
+
+
+class Portfolio
+  def initialize(*stocks)
+    @stocks = stocks
   end
 end
